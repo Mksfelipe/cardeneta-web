1b8c4669b7d0b8f4a931d9d79654d2af229e84ae
Felipe Sousa <sousafelipe123@gmail.com>
2024-03-02 12:18:14 -0300

    Adicionar entrega continua

--- Jenkinsfile
+++ Jenkinsfile
@@ -21,6 +21,7 @@ pipeline {
         }
         stage('Deliver') { 
             steps {
+            	sh 'docker build -t cardeneta-api .'
             	sh 'chmod +x ./jenkins/scripts/deliver.sh'
                 sh './jenkins/scripts/deliver.sh' 
             }
--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,1 +1,16 @@
-echo "TEST"
+#!/bin/bash
+
+# Obtém uma lista de todos os IDs de containers
+container_ids=$(docker ps -aq)
+
+# Itera sobre cada ID de container e os remove
+for id in $container_ids
+do
+    docker stop $id  # Para o container, se estiver em execução
+    docker rm $id    # Remove o container
+done
+
+echo "Todos os containers foram removidos com sucesso."
+
+
+docker run -d -p 9090:9090 cardeneta-api
