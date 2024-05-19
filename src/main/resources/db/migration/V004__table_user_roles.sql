6ccfc2a122105639bf43236621ad1d72c22e1b07
Felipe Sousa <sousafelipe123@gmail.com>
2024-05-19 15:57:49 -0300

    teste

--- src/main/resources/db/migration/V004__table_user_roles.sql
+++ src/main/resources/db/migration/V004__table_user_roles.sql
@@ -7,7 +7,9 @@
 CREATE TABLE cardeneta.user_roles (
     role_id bigint NOT NULL,
     user_id bigint NOT NULL,
-    CONSTRAINT cardeneta.user_roles_pkey PRIMARY KEY (role_id, user_id)
+    CONSTRAINT cardeneta.user_roles_pkey PRIMARY KEY (role_id, user_id),
+    FOREIGN KEY (role_id) REFERENCES cardeneta.roles(id),
+    FOREIGN KEY (user_id) REFERENCES cardeneta.users(id),
 );
 
 
